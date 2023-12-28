9407409f0be7a0f4131b799c644f61b142ba553d
Alejandro Larios Acevedo <alarios@cbps.canon.com>
2023-12-28 11:30:54 -0800

    ActivatedRoute added

--- src/app/products/products-details/products-details.component.ts
+++ src/app/products/products-details/products-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-products-details',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './products-details.component.css'
 })
 export class ProductsDetailsComponent {
+  
+  constructor(private route: ActivatedRoute){
+  }
 
+  ngOnInit() {
+    //const productId = Number(this.route.snapshot.paramMap.get('productId'));
+    this.route.paramMap.subscribe(params => {
+      const productId = Number(params.get('productId')) ;
+
+      if (productId > 0) {
+        console.log("productId: " + productId);
+      }
+      else {
+        
+      }
+    });
+    
+  }
 }
